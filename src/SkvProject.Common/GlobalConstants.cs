50335b9ee0fac878ab63fbce21963364a6021d4b
Stivan Kostadinov <stivan.kostadinov@gmail.com>
2022-05-13 12:30:50 +0300

    Initial commit

---
+++ src/SkvProject.Common/GlobalConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace SkvProject.Common
+{
+    public static class GlobalConstants
+    {
+        public const string SystemName = "SkvProject";
+
+        public const string AdministratorRoleName = "Administrator";
+    }
+}
---
+++ src/Web/SkvProject.Web.ViewModels/Settings/SettingsListViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace SkvProject.Web.ViewModels.Settings
+{
+    using System.Collections.Generic;
+
+    public class SettingsListViewModel
+    {
+        public IEnumerable<SettingViewModel> Settings { get; set; }
+    }
+}
