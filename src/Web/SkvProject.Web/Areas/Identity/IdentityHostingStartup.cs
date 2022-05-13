3a085bd9423763187807b4c8d9b73d801d34fcea
Stivan Kostadinov <stivan.kostadinov@gmail.com>
2022-05-13 17:08:42 +0300

    Insert identity views

---
+++ src/Web/SkvProject.Web/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SkvProject.Data;
+using SkvProject.Data.Models;
+
+[assembly: HostingStartup(typeof(SkvProject.Web.Areas.Identity.IdentityHostingStartup))]
+namespace SkvProject.Web.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
