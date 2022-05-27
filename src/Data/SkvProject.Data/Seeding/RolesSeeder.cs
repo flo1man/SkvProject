03a387977343cce244af036d5815b0bc078c9f57
Stivan Kostadinov <stivan.kostadinov@gmail.com>
2022-05-27 23:38:37 +0300

    Seed administration role to user

--- src/Data/SkvProject.Data/Seeding/RolesSeeder.cs
+++ src/Data/SkvProject.Data/Seeding/RolesSeeder.cs
@@ -4,19 +4,35 @@
     using System.Linq;
     using System.Threading.Tasks;
 
-    using SkvProject.Common;
-    using SkvProject.Data.Models;
-
     using Microsoft.AspNetCore.Identity;
     using Microsoft.Extensions.DependencyInjection;
+    using SkvProject.Data.Models;
+
+    using static SkvProject.Common.GlobalConstants;
 
     internal class RolesSeeder : ISeeder
     {
+        private const string AdminUsername = "admin@admin.com";
+
         public async Task SeedAsync(ApplicationDbContext dbContext, IServiceProvider serviceProvider)
         {
             var roleManager = serviceProvider.GetRequiredService<RoleManager<ApplicationRole>>();
 
-            await SeedRoleAsync(roleManager, GlobalConstants.AdministratorRoleName);
+            await SeedRoleAsync(roleManager, AdministratorRoleName);
+
+            if (!dbContext.UserRoles.Any())
+            {
+                var admin = dbContext.Users.FirstOrDefault(x => x.UserName == AdminUsername);
+
+                admin.Roles.Add(new IdentityUserRole<string>()
+                {
+                    UserId = admin.Id,
+                    RoleId = dbContext.Roles
+                        .FirstOrDefault(x => x.Name == AdministratorRoleName)?.Id,
+                });
+
+                await dbContext.SaveChangesAsync();
+            }
         }
 
         private static async Task SeedRoleAsync(RoleManager<ApplicationRole> roleManager, string roleName)
